1f5802c89c1bff1d5e933c67ca53be613bd59d23
Kevin Jensen <kevin@riskcanvas.com>
2023-11-17 17:28:07 -0700

    Added router components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { Component } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { TextPoolComponent } from "./text-pool/text-pool.component";
+import { MemeComponent } from "./meme/meme.component";
+import { BattlescreenComponent } from "./battlescreen/battlescreen.component";
+import { StartScreenComponent } from "./start-screen/start-screen.component";
+import { HomeScreenComponent } from "./home-screen/home-screen.component";
+import { NewGameComponent } from './new-game/new-game.component';
+import { JoinGameComponent } from './join-game/join-game.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+    {'path': 'new-game', component: NewGameComponent},
+    {'path': 'join-game', component: JoinGameComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TextPoolComponent,
+        MemeComponent,
+        BattlescreenComponent,
+        StartScreenComponent,
+        HomeScreenComponent,
+        NewGameComponent,
+        JoinGameComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot(
+            appRoutes, {enableTracing: true},
+        ),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
